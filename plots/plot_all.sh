52ae29082160dfca25a616bd7d23409fe041db08
Vivian Miranda <vivianmiranda@arizona.edu>
2023-08-24 08:58:15 -0700

    Update plots

--- plots/plot_all.sh
+++ plots/plot_all.sh
@@ -4,3 +4,5 @@ export OUTPUT_PIP_2="/dev/null"
 python plot1.py > ${OUTPUT_PIP_1} 2> ${OUTPUT_PIP_2} 
 
 python plot2.py > ${OUTPUT_PIP_1} 2> ${OUTPUT_PIP_2}
+
+python plot3.py > ${OUTPUT_PIP_1} 2> ${OUTPUT_PIP_2}
